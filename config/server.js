68379220c6cffaa03a17d72c9afcd4e555c8c1a5
Aleksandr Mizin <mizzzin@icloud.com>
2021-06-24 13:24:51 +0300

    clean installed strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0423979937c5f7522121c814faca543d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2d6b72ee-d2fe-49d3-b737-02f0a0e721e9'
+};
