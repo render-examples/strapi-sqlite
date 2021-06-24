73094be1f6fbff030960265c3f42d3986272e65d
Aleksandr Mizin <mizzzin@icloud.com>
2021-06-24 16:52:43 +0300

    change DB path

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", "db/data.db"),
       },
       options: {
         useNullAsDefault: true,
