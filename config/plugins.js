c1e8ef2a3d326a3326701d2715c9a173dae1153f
Aleksandr Mizin <mizzzin@icloud.com>
2021-06-24 17:11:15 +0300

    added graphql config

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = {
+  //
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 7,
+    amountLimit: 100,
+    apolloServer: {
+      tracing: false,
+    },
+  },
+};
