9f8f099e6facfa8dfc8241993702418d7791cbf8
bufan <bufan@hotmail.com>
2022-07-14 13:58:30 +0800

    remove APP_KEYS in config

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: ['M3Fe26SlWSqsVxdYpl1jXQaDGJTAFi3P']
+    keys: env.array('APP_KEYS')
   },
 });
