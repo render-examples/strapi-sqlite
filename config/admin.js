089a3ff116dbc025d367b9c31b99b56fa961601d
bufan <bufan@hotmail.com>
2022-07-14 14:43:28 +0800

    add API_TOKEN_SALT

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
+  },
 });
