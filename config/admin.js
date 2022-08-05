e1b8a50b5ce525e972506056b04d43ac1f61fe15
spirobel <43391631+spirobel@users.noreply.github.com>
2022-08-05 19:49:51 +0800

    add apiToken.salt variable to config/admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT','b4dbad35-6caa-4f84-be56-90d0d3bc2fdf')
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
   },
